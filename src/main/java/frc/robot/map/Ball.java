f62dc81015e44dd4316a6c9a100f011a3fb09b22
ctay22 <ctay22@concordcarlisle.org>
2022-02-11 14:08:05 -0500

    Ball groundbreaking

---
+++ src/main/java/frc/robot/map/Ball.java
@@ -0,0 +1,12 @@
+package frc.robot.map;
+
+public class Ball {
+    public Ball() {
+        // Ball detection is done by coprocessor vision processing
+        // Balls have 3pos and 3vel, and detected
+        // all 22 slots are being emitted
+        // have a detected time, if detected time > 3 secs or strays inside obstacle
+        // cell, the ball object is defuncted
+
+    }
+}
