da808f8c0895f4ef2bdd992ca516596ef168c342
ctay22 <ctay22@concordcarlisle.org>
2022-03-03 12:06:30 -0500

    shooter storage 2 voltage percent debug

--- src/main/java/frc/robot/commands/subsystems/Shooter.java
+++ src/main/java/frc/robot/commands/subsystems/Shooter.java
@@ -5,6 +5,8 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.HardwareObjects;
 import frc.robot.Constants;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Shooter {
     public Shooter() {
 
@@ -14,5 +16,7 @@ public class Shooter {
         hardware.STORAGE_2_MOTOR.set(ControlMode.PercentOutput, storage_2 * -1);
         hardware.SHOOTER_1_MOTOR.set(ControlMode.PercentOutput, shooter1 * -1);
         hardware.SHOOTER_2_MOTOR.set(ControlMode.PercentOutput, shooter2 * -1);
+        SmartDashboard.putNumber("Shooter/Storage V", hardware.STORAGE_2_MOTOR.getMotorOutputVoltage());
+        SmartDashboard.putNumber("Shooter/Storage P", hardware.STORAGE_2_MOTOR.getMotorOutputPercent());
     }
 }
