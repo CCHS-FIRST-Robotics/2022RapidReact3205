e9384a6dfd94bd9ee5341563780b218c82106719
ctay22 <ctay22@concordcarlisle.org>
2022-03-14 14:12:07 -0400

    set read state

--- src/main/java/frc/robot/network/LimeNet.java
+++ src/main/java/frc/robot/network/LimeNet.java
@@ -4,14 +4,23 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class Limelight {
+public class LimeNet {
 
     NetworkTableEntry tx;
     NetworkTableEntry ty;
     NetworkTableEntry tv;
+    NetworkTableEntry led_mode;
+    NetworkTableEntry cam_mode;
+    NetworkTableEntry pipeline;
 
-    public Limelight() {
+    public LimeNet() {
 
+    }
+
+    public void setReadState() {
+        this.led_mode.setNumber(2);
+        this.cam_mode.setNumber(0);
+        this.pipeline.setNumber(0);
     }
 
     public void init(NetworkTableInstance inst) {
@@ -19,9 +28,13 @@ public class Limelight {
         this.tx = limelight.getEntry("tx");
         this.ty = limelight.getEntry("ty");
         this.tv = limelight.getEntry("tv");
+        this.led_mode = limelight.getEntry("ledMode");
+        this.cam_mode = limelight.getEntry("camMode");
+        this.pipeline = limelight.getEntry("pipeline");
     }
 
     public boolean getValid() {
+        setReadState();
         int val = (int) (this.tv.getDouble(0) + 0.01);
         if (val == 0) {
             return false;
@@ -30,6 +43,7 @@ public class Limelight {
     }
 
     public double[] getAngles() {
+        setReadState();
         double[] ang_arr = { tx.getDouble(0), ty.getDouble(0) };
         return ang_arr;
     }
